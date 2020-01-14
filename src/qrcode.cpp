4862ee6f0b18f45911747d4c1469faf3f7a42799
yoprogramo <yoprogramo@gmail.com>
2020-01-14 17:28:38 +0100

    First version including ST7735

--- src/qrcode.cpp
+++ src/qrcode.cpp
@@ -10,15 +10,28 @@ bool QRDEBUG = false;
 
 QRcode::QRcode(OLEDDisplay *display){
 	this->display = display;
+  this->model = -1;
+}
+
+QRcode::QRcode(Adafruit_ST7735 *tft, uint8_t model){
+	this->tft = tft;
+  this->model = model;
+  offsetsX = 20;
+  offsetsY = 20;
 }
 
 void QRcode::init(){
-	if(QRDEBUG)
-		Serial.println("QRcode init");
-
-	display->init();
-  display->flipScreenVertically();
-	display->setColor(WHITE);
+	if (this->model==-1){
+    display->init();
+    display->flipScreenVertically();
+    display->setColor(WHITE);
+  } else {
+    screenwidth = tft->width();
+    screenheight = tft->height(); 
+    tft->initR(model);
+    tft->setRotation(1);
+    tft->fillScreen(ST77XX_WHITE);
+  }
 }
 
 void QRcode::debug(){
@@ -26,23 +39,48 @@ void QRcode::debug(){
 }
 
 void QRcode::render(int x, int y, int color){
-  x=x+offsetsX;
-  y=y+offsetsY;
+  int multiply = 1;
+  if (model != -1)
+    multiply = 2;
+  x=(x*multiply)+offsetsX;
+  y=(y*multiply)+offsetsY;
   if(color==1) {
-	display->setColor(BLACK);
-    display->setPixel(x, y);
+    if (model==-1) {
+	    display->setColor(BLACK);
+      display->setPixel(x, y);
+    } else {
+      tft->drawPixel(x,y,ST77XX_BLACK);
+      if (multiply>1) {
+        tft->drawPixel(x+1,y,ST77XX_BLACK);
+        tft->drawPixel(x+1,y+1,ST77XX_BLACK);
+        tft->drawPixel(x,y+1,ST77XX_BLACK);
+      }
+    }
   }
   else {
-	display->setColor(WHITE);
-    display->setPixel(x, y);
+    if (model==-1) {
+	    display->setColor(WHITE);
+      display->setPixel(x, y);
+    } else {
+      tft->drawPixel(x,y,ST77XX_WHITE);
+      if (multiply>1) {
+        tft->drawPixel(x+1,y,ST77XX_WHITE);
+        tft->drawPixel(x+1,y+1,ST77XX_WHITE);
+        tft->drawPixel(x,y+1,ST77XX_WHITE);
+      }
+    }
   }
 }
 
 void QRcode::screenwhite(){
-	display->clear();
-	display->setColor(WHITE);
-	display->fillRect(0, 0, screenwidth, screenheight);
-	display->display();
+   if (model==-1) {
+      display->clear();
+      display->setColor(WHITE);
+      display->fillRect(0, 0, screenwidth, screenheight);
+      display->display();
+   } else {
+      tft->fillScreen(ST77XX_WHITE);
+   }
 }
 
 void QRcode::create(String message) {
@@ -52,44 +90,32 @@ void QRcode::create(String message) {
   qrencode();
   screenwhite();
 
-  if(QRDEBUG){
-	Serial.println("QRcode render");
-	Serial.println();
-  }
+  
   // print QR Code
   for (byte x = 0; x < WD; x+=2) {
     for (byte y = 0; y < WD; y++) {
       if ( QRBIT(x,y) &&  QRBIT((x+1),y)) {
         // black square on top of black square
-        if(QRDEBUG)
-			Serial.print("@");
         render(x, y, 1);
         render((x+1), y, 1);
       }
       if (!QRBIT(x,y) &&  QRBIT((x+1),y)) {
         // white square on top of black square
-        if(QRDEBUG)
-			Serial.print(" ");
         render(x, y, 0);
         render((x+1), y, 1);
       }
       if ( QRBIT(x,y) && !QRBIT((x+1),y)) {
         // black square on top of white square
-        if(QRDEBUG)
-			Serial.print("@");
         render(x, y, 1);
         render((x+1), y, 0);
       }
       if (!QRBIT(x,y) && !QRBIT((x+1),y)) {
         // white square on top of white square
-        if(QRDEBUG)
-			Serial.print(" ");
         render(x, y, 0);
         render((x+1), y, 0);
       }
     }
-    Serial.println();
   }
-  Serial.println();
-  display->display();
+  if (model==-1)
+    display->display();
 }
--- src/qrcode.h
+++ src/qrcode.h
@@ -1,16 +1,22 @@
 #include "OLEDDisplay.h"
+#include <Adafruit_GFX.h>      // include Adafruit graphics library
+#include <Adafruit_ST7735.h>   // include Adafruit ST7735 TFT library
 
 class QRcode
 {
 	private:
 		OLEDDisplay *display;
+		Adafruit_ST7735 *tft;
+		uint8_t model;
 		void render(int x, int y, int color);
 
 	public:
 		QRcode(OLEDDisplay *display);
+		QRcode(Adafruit_ST7735 *display, uint8_t model);
 
 		void init();
 		void debug();
 		void screenwhite();
 		void create(String message);
+		
 };
